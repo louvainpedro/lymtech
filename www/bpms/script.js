34993fc7dfee62dc634454ec9d0a629d4373440f
Saullo <saulloreis01@hotmail.com>
2024-05-23 10:45:18 -0300

    layout da navbar e funcionalidade pronta, faltando apenas inserção de links e estilos

---
+++ www/bpms/script.js
@@ -0,0 +1,16 @@
+let nav2 = document.getElementById("nav2");
+let services = document.getElementById("services");
+let insights = document.getElementById("insights");
+
+function ShowNav2() {
+    nav2.style.display = "flex";
+    nav2.style.flexDirection = "row";
+    nav2.style.position = "absolute";
+}
+
+function HideNav2() {
+    nav2.style.display = "none";
+}
+
+services.addEventListener("mouseover", ShowNav2);
+services.addEventListener("mouseout", HideNav2);
