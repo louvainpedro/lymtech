2ce3a877ae78305b86018d92e95c75ee0cc65680
Leonardo Mendonça <leonardo.mendonca@lymtech.com.br>
2024-06-14 17:05:47 -0300

    Adicionando traduções de portugues e espanhol para pagina Home e portugues para a pagina bonita

--- www/languages/i18n.js
+++ www/languages/i18n.js
@@ -20,7 +20,24 @@ function changeLanguage(lang) {
                 }
             });
         })
-        .catch(error => console.error(`Error: ${error}`));
+        .catch(error => {
+            console.error(`Error fetching from first path: ${error}`);
+            fetch(`../languages/${lang}.json`)
+                .then(response => response.json())
+                .then(translations => {
+                    document.querySelectorAll('[data-i18n]').forEach(element => {
+                        const key = element.getAttribute('data-i18n');
+                        const value = translations[key];
+
+                        if (element.getAttribute('data-i18n-placeholder') !== null) {
+                            element.setAttribute('placeholder', value);
+                        } else {
+                            element.textContent = value;
+                        }
+                    });
+                })
+                .catch(secondError => console.error(`Error fetching from second path: ${secondError}`));
+        });
 }
 
 // Chama getUserLanguage quando a página é carregada para aplicar o idioma selecionado
