09cc5de8e70cc3805299c34b73a51ce13af6e566
eduardo_zelaya <eduardo.zelaya@lymtech.com.br>
2024-06-14 15:56:00 -0300

    salvar alteracoes

--- www/languages/i18n.js
+++ www/languages/i18n.js
@@ -2,21 +2,17 @@ function getUserLanguage() {
     const lang = localStorage.getItem('selectedLanguage') || (navigator.language || navigator.userLanguage || 'en').split('-')[0];
     changeLanguage(lang);
 }
-
+ 
 function changeLanguage(lang) {
     localStorage.setItem('selectedLanguage', lang);
-
-<<<<<<< HEAD
+ 
     fetch(`./languages/z${lang}.json`)
-=======
-    fetch(`./languages/${lang}.json`)
->>>>>>> a86eaf0f895aac2e928915ec3965ce1a82d194a6
         .then(response => response.json())
         .then(translations => {
             document.querySelectorAll('[data-i18n]').forEach(element => {
                 const key = element.getAttribute('data-i18n');
                 const value = translations[key];
-
+ 
                 if (element.getAttribute('data-i18n-placeholder') !== null) {
                     element.setAttribute('placeholder', value);
                 } else {
@@ -24,8 +20,25 @@ function changeLanguage(lang) {
                 }
             });
         })
-        .catch(error => console.error(`Error: ${error}`));
+        .catch(error => {
+            console.error(`Error fetching from first path: ${error}`);
+            fetch(`../languages/z${lang}.json`)
+                .then(response => response.json())
+                .then(translations => {
+                    document.querySelectorAll('[data-i18n]').forEach(element => {
+                        const key = element.getAttribute('data-i18n');
+                        const value = translations[key];
+ 
+                        if (element.getAttribute('data-i18n-placeholder') !== null) {
+                            element.setAttribute('placeholder', value);
+                        } else {
+                            element.textContent = value;
+                        }
+                    });
+                })
+                .catch(secondError => console.error(`Error fetching from second path: ${secondError}`));
+        });
 }
-
+ 
 // Chama getUserLanguage quando a página é carregada para aplicar o idioma selecionado
 document.addEventListener('DOMContentLoaded', getUserLanguage);
